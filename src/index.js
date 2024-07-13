1ecd79a6c3f423fb8bd7bc0ed16eb45a12c5dc9d
Jason Chan <74099202+apooridiot@users.noreply.github.com>
2024-07-13 11:53:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.apooridiot.xyz": dockerHub,
+  "quay.apooridiot.xyz": "https://quay.io",
+  "gcr.apooridiot.xyz": "https://gcr.io",
+  "k8s-gcr.apooridiot.xyz": "https://k8s.gcr.io",
+  "k8s.apooridiot.xyz": "https://registry.k8s.io",
+  "ghcr.apooridiot.xyz": "https://ghcr.io",
+  "cloudsmith.apooridiot.xyz": "https://docker.cloudsmith.io",
+  "ecr.apooridiot.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.apooridiot.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
